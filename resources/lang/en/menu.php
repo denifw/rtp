a9db4302cac4da57fcfa643d08ba9c736ef72b9b
Deni Firdaus Waruwu <deni.fw@mbteknologi.com>
2021-06-08 16:48:24 +0700

    Adding english translation

---
+++ resources/lang/en/menu.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'root' => 'Root',
+    'statistic' => 'Statistic',
+    'crm' => 'CRM',
+    'operation' => 'Operational',
+    'administration' => 'Administration',
+    'master' => 'Master',
+    'system' => 'System',
+    'access' => 'Access',
+    'document' => 'Document',
+    'page' => 'Page',
+    'finance' => 'Finance',
+];
